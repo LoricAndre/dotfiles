2066bf528a4fcc88e2f11cf08598b1fee174a49d
Loric ANDRE <loric.a@padoa-group.com>
2022-04-26 15:50:43 +0200

    switch all plugins to yaml config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,51 +1,49 @@
 local vim = vim
-local paq = require'utils'.paq
+local packer = require'utils'.packer
 local config = require'config'
 
 local defaults = {
-  'savq/paq-nvim',
-
-  oneterm = 'LoricAndre/OneTerm.nvim',
-  treesitter = {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'},
-  comment = 'numToStr/Comment.nvim',
-  sniprun = {'michaelb/sniprun', run = 'bash ./install.sh'},
-  indent = 'lukas-reineke/indent-blankline.nvim',
-  sidebar = 'sidebar-nvim/sidebar.nvim',
-  commentBox = 'LudoPinelli/comment-box.nvim',
-  feline = {'famiu/feline.nvim', requires = {
-    'kyazdani42/nvim-web-devicons',
-    'lewis6991/gitsigns.nvim'
-  }},
-  coq = {'ms-jpq/coq_nvim', requires = {
-      {'ms-jpq/coq.artifacts', branch = 'artifacts'},
-      {'ms-jpq/coq.thirdparty'}
-    }
-  },
-  lsp = {'williamboman/nvim-lsp-installer', requires = {
-    'neovim/nvim-lspconfig',
-    'jubnzv/virtual-types.nvim',
-    'weilbith/nvim-code-action-menu',
-    'kosayoda/nvim-lightbulb',
-    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-  }},
-  alpha = 'goolord/alpha-nvim',
-  'haringsrob/nvim_context_vt',
-  'ggandor/lightspeed.nvim',
-  persistence = 'folke/persistence.nvim',
-  'neomake/neomake',
-  'lewis6991/impatient.nvim',
-  'shaunsingh/nord.nvim',
-  'folke/tokyonight.nvim',
-  'tiagovla/tokyodark.nvim',
-  catpuccin = 'catppuccin/nvim'
+  -- oneterm = 'LoricAndre/OneTerm.nvim',
+  -- treesitter = {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'},
+  -- comment = 'numToStr/Comment.nvim',
+  -- sniprun = {'michaelb/sniprun', run = 'bash ./install.sh'},
+  -- indent = 'lukas-reineke/indent-blankline.nvim',
+  -- sidebar = 'sidebar-nvim/sidebar.nvim',
+  -- commentBox = 'LudoPinelli/comment-box.nvim',
+  -- feline = {'famiu/feline.nvim', requires = {
+  --   'kyazdani42/nvim-web-devicons',
+  --   'lewis6991/gitsigns.nvim'
+  -- }},
+  -- coq = {'ms-jpq/coq_nvim', requires = {
+  --     {'ms-jpq/coq.artifacts', branch = 'artifacts'},
+  --     {'ms-jpq/coq.thirdparty'}
+  --   }
+  -- },
+  -- lsp = {'williamboman/nvim-lsp-installer', requires = {
+  --   'neovim/nvim-lspconfig',
+  --   'jubnzv/virtual-types.nvim',
+  --   'weilbith/nvim-code-action-menu',
+  --   'kosayoda/nvim-lightbulb',
+  --   'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+  -- }},
+  -- alpha = 'goolord/alpha-nvim',
+  -- {'haringsrob/nvim_context_vt', enabled = false},
+  -- 'ggandor/lightspeed.nvim',
+  -- persistence = 'folke/persistence.nvim',
+  -- 'neomake/neomake',
+  -- 'lewis6991/impatient.nvim',
+  -- 'shaunsingh/nord.nvim',
+  -- 'folke/tokyonight.nvim',
+  -- 'tiagovla/tokyodark.nvim',
+  -- catpuccin = 'catppuccin/nvim'
 }
 
 for name, desc in pairs(config.plugins) do
   if type(name) == 'string' then
-    table.insert(defaults, name, desc)
+    defaults[name] = desc
   else
     table.insert(defaults, desc)
   end
 end
 
-paq(defaults)
+packer(defaults)
--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -33,65 +33,30 @@ M.set = function(options)
   end
 end
 
-local paq_make_url = function(item)
-  if type(item) ~= 'string' then
-    item = item[1]
-  end
-  local url = item
-  local _, slashes = string.gsub(item, "/", "")
-  if slashes == 1 then
-    url = 'git@github.com:' .. item
-  end
-  return url
-end
-
-local paq_deps = function(config, deps)
-  if deps then
-    for _, a in ipairs(deps) do
-      local item = {url = paq_make_url(a)}
-      table.insert(config, item)
-    end
-  end
-  return config
-end
-
-M.paq = function(plugins)
-  local config = {}
-  for name, desc in pairs(plugins) do
-    config[name] = {}
-    config[name].url = paq_make_url(desc)
-    if type(desc) ~= 'string' then
-      config = paq_deps(config, desc.requires)
-    end
-    if type(name) == 'string' then
-      config[name].as = name
-      if vim.api.nvim_get_runtime_file("*/plugins/" .. name ..".lua", true) ~= {} then
-        if not pcall(function() require('plugins.' .. name) end) then
-          print("Error while sourcing config for " .. name)
-        end
-
-      end
-    end
-  end
-  return require'paq'(config)
-end
-
 M.packer = function(plugins)
   local packer = require'packer'
+  local opts = {}
+  packer.reset()
+  packer.init()
   for name, desc in pairs(plugins) do
-    local plugin = desc
     if type(desc) == 'string' then
-      plugin = {desc}
+      table.insert(opts, desc)
     end
     if type(name) == 'string' then
-      plugin.as = name
-      if vim.api.nvim_get_runtime_file("*/plugins/" .. name ..".lua", true) ~= {} then
-        if not pcall(function() require('plugins.' .. name) end) then
-          print("Error while sourcing config for " .. name)
+      local plugin = desc
+      if type(plugin) == 'string' then
+        plugin = {plugin}
+      end
+      if plugin.enabled ~= false then
+        plugin.as = name
+        plugin.disable = not plugin.enabled
+        if table.getn(vim.api.nvim_get_runtime_file("*/plugins/" .. name ..".lua", true)) > 0 then
+          require('plugins.' .. name)
         end
+        -- table.insert(opts, 1, plugin)
+        packer.use(plugin)
       end
     end
-    packer.use(plugin)
   end
 end
 
