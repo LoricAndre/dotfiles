2df35aa24aa8b0f55798ba5eb31ce3605601beba
Loric ANDRE <loric.a@padoa-group.com>
2022-04-27 14:34:17 +0200

    Cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,2 @@
 require'impatient'
-
-local vim = vim
-local u = require'utils'
-
-config = u.getConfig('config.yaml')
-
-u.let(config.globals)
-u.set(config.options)
-u.map(config.mappings)
-u.aug("Init", config.autocmds)
-
-u.packer(config.plugins)
-
-u.colorscheme(config.colorscheme)
+require'ynvim'.setup('config.yaml')
