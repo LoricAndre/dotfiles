9a3334da907c393424ecbeef187ddc674d17bcbb
Loric André <gitlab.j3bzo@slmail.me>
2024-06-16 13:29:12 +0200

    fix multilib

--- .chezmoiscripts/run_once_before_install_paru.sh
+++ .chezmoiscripts/run_once_before_install_paru.sh
@@ -6,5 +6,7 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si --noconfirm
 
-echo "[multilib]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/mirrorlist"
+paru -R --noconfirm paru-debug || true
+
+echo "[multilib]" | sudo tee --append /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" | sudo tee --append /etc/pacman.conf
