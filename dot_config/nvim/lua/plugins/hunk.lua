cc01a8323351a80b88d92b8336e75b9230532abd
loric.andre <gitlab.j3bzo@slmail.me>
2024-08-20 22:30:41 +0200

    Fix hunk config

--- dot_config/nvim/lua/plugins/hunk.lua
+++ dot_config/nvim/lua/plugins/hunk.lua
@@ -1,5 +1,54 @@
 return {
   "julienvincent/hunk.nvim",
   cmd = { "DiffEditor" },
-  opts = {}
+  opts = {
+    keys = {
+      global = {
+        quit = { "q" },
+        accept = { "<leader><Cr>" },
+        focus_tree = { "<leader>e" },
+      },
+
+      tree = {
+        expand_node = { "m", "<Right>" },
+        collapse_node = { "j", "<Left>" },
+
+        open_file = { "<Cr>" },
+
+        toggle_file = { "a" },
+      },
+
+      diff = {
+        toggle_line = { "a" },
+        toggle_hunk = { "A" },
+      },
+    },
+
+    ui = {
+      tree = {
+        -- Mode can either be `nested` or `flat`
+        mode = "nested",
+        width = 35,
+      },
+      --- Can be either `vertical` or `horizontal`
+      layout = "vertical",
+    },
+
+    icons = {
+      selected = "󰡖",
+      deselected = "",
+      partially_selected = "󰛲",
+
+      folder_open = "",
+      folder_closed = "",
+    },
+
+    -- Called right after each window and buffer are created.
+    hooks = {
+      ---@param _context { buf: number, tree: NuiTree, opts: table }
+      on_tree_mount = function(_context) end,
+      ---@param _context { buf: number, win: number }
+      on_diff_mount = function(_context) end,
+    },
+  }
 }
