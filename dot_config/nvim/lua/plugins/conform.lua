eae172ce20cee8364b134d24341eb1dd4283aa6b
Loric André <gitlab.j3bzo@slmail.me>
2024-10-16 18:05:43 +0200

    update conform for react

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -14,6 +14,7 @@ return {
         lua = { 'stylua' },
         typescript = { 'prettierd' },
         javascript = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
         rust = { 'rustfmt' }
       },
     },
