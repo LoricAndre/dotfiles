91dc43984a551cc1ad0082e014207ae4773f016d
LoricAndre <loric.andre@pm.me>
2024-11-04 09:39:06 +0100

    misc

---
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,19 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-neotest/neotest-python',
+  },
+  config = function()
+    require('neotest').setup({
+      adapters = {
+        require('neotest-python')({
+          runner = 'unittest',
+        }),
+      },
+    })
+  end,
+}
