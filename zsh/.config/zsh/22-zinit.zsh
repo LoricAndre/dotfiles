f14307e080e4b2256a42df8eddd95ac06a9438c6
LoricAndre <loric.andre@pm.me>
2025-04-23 17:53:32 +0200

    fix(zinit): fix zinit install path

--- zsh/.config/zsh/22-zinit.zsh
+++ zsh/.config/zsh/22-zinit.zsh
@@ -1,5 +1,10 @@
 [ -f /usr/share/zinit/zinit.zsh ] && source /usr/share/zinit/zinit.zsh
-[ -f $HOME/.local/share/zinit/zinit.zsh ] && source $HOME/.local/share/zinit/zinit.zsh
+[ -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ] && source $HOME/.local/share/zinit/zinit.git/zinit.zsh
+
+if ! command -v zinit >/dev/null 2>&1; then
+	echo '[zsg/apply-hook] zinit not found'
+	exit 1;
+fi
 
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
