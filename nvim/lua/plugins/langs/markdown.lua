ea3aefe445027845b5ec7296f921d3765ebd0c88
LoricAndre <loric.andre@pm.me>
2025-04-25 16:09:06 +0200

    fix: remove markview wiki

--- nvim/lua/plugins/langs/markdown.lua
+++ nvim/lua/plugins/langs/markdown.lua
@@ -2,6 +2,7 @@ return {
   {
     'OXY2DEV/markview.nvim',
     enabled = true,
+    submodules = false,
     ft = { 'markdown', 'codecompanion', 'avante' },
     opts = {
       preview = { filetypes = { 'markdown', 'codecompanion', 'avante' } },
