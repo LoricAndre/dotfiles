b2182c3fd94d03669eefe6a1cefb568f010df3b5
LoricAndre <loric.andre@pm.me>
2025-06-02 23:19:56 +0200

    feat(nvim): better lsp mappings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -64,31 +64,67 @@ return {
           vim.notify('Failed to setup client attach cb', vim.log.levels.WARN)
           return
         end
-        vim.keymap.set({ 'n', 'v' }, 'gh', function()
-          vim.diagnostic.jump({ count = -1, float = true })
-        end, { desc = '[lsp] jump to previous diagnostic' })
-        vim.keymap.set({ 'n', 'v' }, 'gl', function()
-          vim.diagnostic.jump({ count = 1, float = true })
-        end, { desc = '[lsp] jump to next diagnostic' })
-        vim.keymap.set({ 'n' }, '<leader>cn', function()
-          vim.lsp.buf.rename()
-        end, { desc = '[lsp] rename' })
-        -- vim.keymap.set({ 'n', 'v' }, '<leader>cf', function() vim.lsp.buf.format({async = true, id = client, bufnr = bufnr}) end)
-        vim.keymap.set({ 'n' }, '<leader>cd', function()
-          vim.lsp.buf.definition({ reuse_win = true })
-        end, { desc = '[lsp] definition' })
-        vim.keymap.set({ 'n' }, '<leader>cD', function()
-          vim.lsp.buf.declaration({ reuse_win = true })
-        end, { desc = '[lsp] declaration' })
-        vim.keymap.set({ 'n' }, '<leader>ct', function()
-          vim.lsp.buf.type_definition({ reuse_win = true })
-        end, { desc = '[lsp] type definition' })
-        vim.keymap.set({ 'n' }, '<leader>ci', function()
-          vim.lsp.buf.implementation({ reuse_win = true })
-        end, { desc = '[lsp] implementation' })
-        vim.keymap.set({ 'n' }, '<leader>ce', function()
-          vim.diagnostic.setqflist()
-        end, { desc = '[lsp] diagnostics' })
+        if client:supports_method('textDocument/diagnostic') then
+          vim.keymap.set({ 'n', 'v' }, 'gh', function()
+            vim.diagnostic.jump({ count = -1, float = true })
+          end, { desc = '[lsp] jump to previous diagnostic' })
+          vim.keymap.set({ 'n', 'v' }, 'gl', function()
+            vim.diagnostic.jump({ count = 1, float = true })
+          end, { desc = '[lsp] jump to next diagnostic' })
+        end
+        if client:supports_method('textDocument/rename') then
+          vim.keymap.set({ 'n' }, '<leader>cn', function()
+            vim.lsp.buf.rename()
+          end, { desc = '[lsp] rename' })
+        end
+        if client:supports_method('textDocument/formatting') then
+          vim.keymap.set('n', '<leader>cf', function()
+            vim.lsp.buf.format({ async = true, id = client.id, bufnr = bufnr })
+          end, { desc = '[lsp] format' })
+          if client:supports_method('textDocument/rangeFormatting') then
+            vim.keymap.set('v', '<leader>cf', function()
+              vim.lsp.buf.format({ async = true, id = client.id, bufnr = bufnr })
+            end, { desc = '[lsp] format' })
+          end
+          if not client:supports_method('textDocument/willSaveWaitUntil') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = aug,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({
+                  bufnr = bufnr,
+                  id = client.id,
+                  timeout_ms = 1000,
+                })
+              end,
+            })
+          end
+        end
+        if client:supports_method('textDocument/definition') then
+          vim.keymap.set({ 'n' }, '<leader>cd', function()
+            vim.lsp.buf.definition({ reuse_win = true })
+          end, { desc = '[lsp] definition' })
+        end
+        if client:supports_method('textDocument/declaration') then
+          vim.keymap.set({ 'n' }, '<leader>cD', function()
+            vim.lsp.buf.declaration({ reuse_win = true })
+          end, { desc = '[lsp] declaration' })
+        end
+        if client:supports_method('textDocument/typeDefinition') then
+          vim.keymap.set({ 'n' }, '<leader>ct', function()
+            vim.lsp.buf.type_definition({ reuse_win = true })
+          end, { desc = '[lsp] type definition' })
+        end
+        if client:supports_method('textDocument/implementation') then
+          vim.keymap.set({ 'n' }, '<leader>ci', function()
+            vim.lsp.buf.implementation({ reuse_win = true })
+          end, { desc = '[lsp] implementation' })
+        end
+        if client:supports_method('workspace/diagnostic') then
+          vim.keymap.set({ 'n' }, '<leader>ce', function()
+            vim.diagnostic.setqflist()
+          end, { desc = '[lsp] diagnostics' })
+        end
         if
           client:supports_method('textDocument/documentColor')
           and vim.lsp.document_color ~= nil
