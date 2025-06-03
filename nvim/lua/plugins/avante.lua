d08f410c255075090c708a76713cd247cc1ca3dc
LoricAndre <loric.andre@pm.me>
2025-06-03 18:21:05 +0200

    fix(nvim): fix avante

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,29 +1,98 @@
 return {
   'yetone/avante.nvim',
-  event = 'VeryLazy',
-  lazy = false,
-  enabled = false,
-  version = '*', -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-  opts = {
-    provider = 'mistral',
-    claude = {
-      api_key_name = 'cmd:pass show anthropic-api-key',
-      disable_tools = {
-        'git_diff',
-        'rag_search',
-        'python',
-        'git_commit',
-        'bash',
-        'web_search',
-      },
-      max_tokens = 4096,
+  lazy = true,
+  -- enabled = false,
+  dependencies = {
+    { 'stevearc/dressing.nvim', lazy = true },
+    { 'nvim-lua/plenary.nvim',  lazy = true },
+    { 'MunifTanjim/nui.nvim',   lazy = true },
+    { 'ibhagwan/fzf-lua',       lazy = true },
+    {
+      'github/copilot.vim',
+      lazy = true,
+      event = 'VeryLazy',
+      cmd = { 'Copilot' },
     },
-    vendors = {
+    { 'nvim-tree/nvim-web-devicons', lazy = true },
+    {
+      'saghen/blink.cmp',
+      lazy = true,
+      opts = {
+        sources = {
+          per_filetype = {
+            avante = {
+              'avante_commands',
+              'avante_mentions',
+              'avante_files',
+            },
+          },
+          providers = {
+            avante_commands = {
+              name = 'avante_commands',
+              module = 'blink.compat.source',
+              score_offset = 90, -- show at a higher priority than lsp
+              opts = {},
+            },
+            avante_files = {
+              name = 'avante_commands',
+              module = 'blink.compat.source',
+              score_offset = 100, -- show at a higher priority than lsp
+              opts = {},
+            },
+            avante_mentions = {
+              name = 'avante_mentions',
+              module = 'blink.compat.source',
+              score_offset = 1000, -- show at a higher priority than lsp
+              opts = {},
+            }
+          }
+        }
+      },
+    },
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+  opts = {
+    provider = 'copilot',
+    selector = {
+      provider = 'fzf_lua',
+      provider_opts = {},
+    },
+    providers = {
       mistral = {
         api_key_name = 'cmd:pass show mistral-api-key',
         model = 'codestral-latest',
         endpoint = 'https://codestral.mistral.ai/v1',
         __inherited_from = 'openai',
+      },
+      claude = {
+        api_key_name = 'cmd:pass show anthropic-api-key',
+        disable_tools = {
+          'git_diff',
+          'rag_search',
+          'python',
+          'git_commit',
+          'bash',
+          'web_search',
+        },
+        extra_request_body = {
+          max_tokens = 4096,
+        },
       },
     },
     hints = {
@@ -62,15 +131,29 @@ return {
     },
   },
   build = 'make',
-  dependencies = {
-    'stevearc/dressing.nvim',
-    'nvim-lua/plenary.nvim',
-    'MunifTanjim/nui.nvim',
-    'ibhagwan/fzf-lua',
-  },
   keys = {
-    { '<leader>aa', '<CMD>AvanteFocus<CR>', desc = '[llm] focus avante' },
-    { '<leader>ac', '<CMR>AvanteChat<CR>', desc = '[llm] open avante chat' },
-    { '<leader>at', '<CMD>AvanteToggle<CR>', desc = '[llm] toggle avante' },
+    { '<leader>aa', '<CMD>AvanteAsk<CR>',         desc = '[llm] ask avante' },
+    { '<leader>af', '<CMD>AvanteFocus<CR>',       desc = '[llm] focus avante' },
+    { '<leader>ac', '<CMR>AvanteChat<CR>',        desc = '[llm] open avante chat' },
+    { '<leader>at', '<CMD>AvanteToggle<CR>',      desc = '[llm] toggle avante' },
+    { '<leader>ah', '<CMD>AvanteHistory<CR>',     desc = '[llm] avante history' },
+    { '<leader>am', '<CMD>AvanteShowRepoMap<CR>', desc = '[llm] avante repo map' },
   },
+  cmd = {
+    'AvanteAsk',
+    'AvanteBuild',
+    'AvanteChatNew',
+    'AvanteHistory',
+    'AvanteClear',
+    'AvanteEdit',
+    'AvanteRefresh',
+    'AvanteStop',
+    'AvanteSwitchProvider',
+    'AvanteShowRepoMap',
+    'AvanteModels',
+    'AvanteSwitchSelectorProvider',
+    'AvanteFocus',
+    'AvanteChat',
+    'AvanteToggle'
+  }
 }
