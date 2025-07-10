4e9120582c24e773f22deaa6763bb0975de68491
loric.andre <gitlab.j3bzo@slmail.me>
2025-07-10 15:32:21 +0200

    feat: back to waybar

--- nvim/lua/plugins/colorschemes/catppuccin.lua
+++ nvim/lua/plugins/colorschemes/catppuccin.lua
@@ -1,21 +1,20 @@
 return {
   'catppuccin/nvim',
   name = 'catppuccin',
-  enabled = vim.g.colorscheme:sub(1, #'catppuccin') == 'catppuccin',
   opts = {
     transparent_background = false, -- disables setting the background color.
-    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-    term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+    term_colors = true,             -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
-      enabled = false, -- dims the background color of inactive window
+      enabled = false,              -- dims the background color of inactive window
       shade = 'dark',
-      percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      percentage = 0.15,            -- percentage of the shade to apply to the inactive window
     },
-    no_italic = false, -- Force no italic
-    no_bold = false, -- Force no bold
-    no_underline = false, -- Force no underline
-    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-      comments = { 'italic' }, -- Change the style of comments
+    no_italic = false,              -- Force no italic
+    no_bold = false,                -- Force no bold
+    no_underline = false,           -- Force no underline
+    styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+      comments = { 'italic' },      -- Change the style of comments
       conditionals = { 'italic' },
       loops = {},
       functions = {},
--- nvim/lua/plugins/colorschemes/tokyonight.lua
+++ nvim/lua/plugins/colorschemes/tokyonight.lua
@@ -3,7 +3,6 @@ return {
   lazy = false,
   priority = 1000,
   opts = {},
-  enabled = vim.g.colorscheme:sub(1, #'tokyonight') == 'tokyonight',
   init = function()
     vim.cmd.colorscheme(vim.g.colorscheme)
   end,
